c286d2166a5ea01e25988e2a2304438b5e5ead0a
Wendy Grant <wendykgrant.tx@gmail.com>
2020-10-27 20:04:42 -0500

    debugged with console.log - app functioning

--- server.js
+++ server.js
@@ -79,14 +79,14 @@ function mainMenu() {
 
 // View all employees 
 function viewAllEmp() {
-
+// console.log("line 82") - checking function - good
   // Query to view all employees
   let query = "SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC";
 
   connection.query(query, function (err, res) {
     if (err) return err;
     console.log("\n");
-
+// console.log(res); checking response - good
     console.table(res);
 
     mainMenu();
